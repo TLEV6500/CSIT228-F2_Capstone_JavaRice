223a528b9a899bb994dea90d299f80014044d7be
rx7 fc3s <jacalanaaronrey@gmail.com>
2025-05-16 19:31:38 +0800

    changed game icon

--- src/main/java/com/example/javarice_capstone/javarice_capstone/Launcher.java
+++ src/main/java/com/example/javarice_capstone/javarice_capstone/Launcher.java
@@ -4,10 +4,12 @@ import com.example.javarice_capstone.javarice_capstone.Multiplayer.XAMPP_Initial
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Objects;
 
 public class Launcher extends Application {
 
@@ -15,6 +17,7 @@ public class Launcher extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Launcher.class.getResource("/com/example/javarice_capstone/javarice_capstone/MenuUI.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1230, 690);
+        stage.getIcons().add(new Image("images/javarice_icon.png"));
         stage.setTitle("PLAY UNO");
         stage.setResizable(false);
         stage.setScene(scene);
